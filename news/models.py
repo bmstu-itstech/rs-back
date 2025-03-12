d5f0de2fcef905e3e7195f0c1db2f4a8d61002df
zhikh <zhikh.k@gmail.com>
2025-03-12 18:01:14 +0300

    chore: add verbose names for model fields

--- news/models.py
+++ news/models.py
@@ -2,20 +2,24 @@ from django.db import models
 
 
 class NewsRecord(models.Model):
-    """
-    Новость, связанная с ЦМР, публикуемая на сайте.
-    """
     title = models.CharField(
+        'Заголовок',
         max_length=100,
         help_text='Заголовок, краткое описание новости. Не более 100 символов',
     )
     content = models.TextField(
+        'Содержание',
         help_text='Текст новости',
     )
     href = models.URLField(
+        'Ссылка',
         help_text='Ссылка на пост в соцсетях с более подробной информацией о новости'
     )
     image = models.ImageField(
+        'Изображение',
         upload_to='news/',
         help_text='Изображение для новости'
     )
+
+    def __str__(self):
+        return self.title

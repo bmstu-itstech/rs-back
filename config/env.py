06dcecf2777fbc45d9027fcf12a49c229edcb366
Елькин Кирилл <74110907+kirillv11@users.noreply.github.com>
2025-05-25 12:46:16 +0300

    chore-002: add .env support and env-config module (#7)
    * chore: add .env variables, update poetry files
    
    * chore: add .env support and env-config module

---
+++ config/env.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+class EnvIsNotDefined(Exception):
+    def __init__(self, key: str):
+        super().__init__(f'environment variable {key} is not defined')
+
+
+def env_required(key: str) -> str:
+    value = os.getenv(key)
+    if not value:
+        raise EnvIsNotDefined(key)
+    return value
+
+
+def env_with_default(key: str, default: str = '') -> str:
+    return os.getenv(key, default)
+
+
+def env_with_default_bool(key: str, default: str = '') -> str:
+    return os.getenv(key, default) == 'True'
+
+
+def env_list_with_default(key: str, default: str = '') -> str:
+    return [
+        value.strip() for value in os.getenv(key, default).split() if value.strip()
+    ]

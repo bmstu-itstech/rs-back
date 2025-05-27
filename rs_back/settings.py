7bdc052d2d6966076db5757165073ca81d1f29dd
zhikh <zhikh.k@gmail.com>
2025-05-27 14:42:53 +0300

    fix: add logging

--- rs_back/settings.py
+++ rs_back/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from config import env
 
+import logging
+logging.basicConfig(filename='error.log',level=logging.DEBUG)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
